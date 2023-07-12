a490086077b651cd40e2bcf18e12eff325ab6a4a
Tanya chopra <tanyachopra28293@gmail.com>
2023-07-12 19:26:16 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// const date = new Date();
+// console.log(date);
+// const hour = date.getHours();
+// document.getElementById("hour").innerHTML = "hour";
+
+// console.log(hour);
+// const min = date.getMinutes();
+// console.log(min);
+// const sec = date.getSeconds();
+// console.log(sec);
